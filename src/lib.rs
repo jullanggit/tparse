677f204537f112f2cff6ed70bfbd3cc673dd95bc
Jul Lang <jullanggit@proton.me>
2025-07-12 23:22:59 +0200

    add Concat

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![feature(adt_const_params)]
 #![feature(unsized_const_params)]
 
-pub struct TStr<const STR: &'static str>();
+/// A type containing a compile-time unique string
+#[derive(Debug, PartialEq)]
+pub struct TStr<const STR: &'static str>;
 
 pub trait TParse
 where
@@ -14,9 +16,11 @@ where
 
 impl<const STR: &'static str> TParse for TStr<STR> {
     fn tparse(input: &str) -> Option<(Self, usize)> {
-        let str = STR;
-        let len = str.len();
-        (&input[0..len] == str).then_some((Self(), len))
+        let len = STR.len();
+        if len > input.len() {
+            return None;
+        }
+        input[0..len].eq(STR).then_some((Self, len))
     }
 }
 
@@ -51,4 +55,54 @@ macro_rules! Or {
             }
         }
     };
+}
+
+/// Match on all arguments followed by each other
+#[macro_export]
+macro_rules! Concat {
+    ($struct:ident, $($ty:ident),+) => {
+        struct $struct($($ty,)+);
+        impl TParse for $struct {
+            fn tparse(input: &str) -> Option<(Self, usize)> {
+                let mut offset = 0;
+
+                $(
+                    let ($ty, new_offset) = $ty::tparse(&input[offset..])?;
+                    offset += new_offset;
+                )+
+
+                Some((Self($($ty,)+), offset))
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_simple_concat() {
+        type test_str = TStr<"test_str">;
+        Concat!(TestStruct, char, test_str);
+
+        let tests = [
+            ("ctest_str", Some((TestStruct('c', TStr), 9))),
+            ("test_str", None),
+            ("\0test_str\0\0\0", Some((TestStruct('\0', TStr), 9))),
+        ];
+
+        for (input, output) in tests {
+            let parsed = TestStruct::tparse(input);
+            match (parsed, output) {
+                (None, None) => {}
+                (Some(parsed), Some(output)) => {
+                    assert_eq!(parsed.0.0, output.0.0);
+                    assert_eq!(parsed.0.1, output.0.1);
+                    assert_eq!(parsed.1, output.1);
+                }
+                _ => panic!(),
+            }
+        }
+    }
 }
